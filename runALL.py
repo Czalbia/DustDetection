f7721b72fcc8799db58fdba3863e1388307e4e50
Jan Cza <58823176+Czalbia@users.noreply.github.com>
2023-08-03 09:56:24 -0700

    Update runALL.py

--- runALL.py
+++ runALL.py
@@ -6,8 +6,10 @@ import argparse
 
 parser = argparse.ArgumentParser(description="The file name of your image")
 
-parser.add_argument('--name', type=str, default='test')
+parser.add_argument('--name', type=str, default='test', help='--name <path to the image you want to use>')
+
+parser.add_argument('--model', type=str, default='solar', help='--model <the path to the model that you want to use>')
 
 args = parser.parse_args()
 
-os.system(f'''/home/nvidia/jetson-inference/build/aarch64/bin/imagenet.py --model=models/solar2/resnet18.onnx --input_blob=input_0 --output_blob=output_0 --labels=data/solar2/labels.txt {args.name} RESULTS__{args.name}''')
+os.system(f'''/home/nvidia/jetson-inference/build/aarch64/bin/imagenet.py --model=models/{args.model}/resnet18.onnx --input_blob=input_0 --output_blob=output_0 --labels=data/{args.model}/labels.txt {args.name} RESULTS__{args.name}''')
